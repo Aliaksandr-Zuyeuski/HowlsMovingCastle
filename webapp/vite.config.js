d6b91f0deb8897badd7b4f83bfc8008661b40363
Aliaksandr <118751798+Aliaksandr-Zuyeuski@users.noreply.github.com>
2026-06-06 13:55:41 +0200

    Update vite.config.js

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../webapp_dist',
+    outDir: 'webapp_dist',
     emptyOutDir: true,
   },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  }
 });
